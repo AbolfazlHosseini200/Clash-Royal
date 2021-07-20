c3f0a967eb78b8dd468c664d19b78691b793758c
Amir Hosseini <abolfazlsobhan@gmail.com>
2021-07-20 20:13:59 +0430

    before adding XP

--- src/sample/Archer.java
+++ src/sample/Archer.java
@@ -3,13 +3,13 @@ package sample;
 import java.net.URL;
 
 public class Archer extends Troops{
-    double number=2;
-    double hitSpd=1.2;
-    double hp=125;
-    double dmg=33;
-    double rng=100;
     Archer(String url) {
         super(125.0,33.0,100.0,url,1);
         speed=0.33;
+    }
+    public void updateXp()
+    {
+        this.xp+=xp;
+        if(xp>)
     }
 }
